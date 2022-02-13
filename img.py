26fa55d4b8af6170d5ddc93cb08c2ffafcfeeb12
dan cooper <88119677+dancoopper@users.noreply.github.com>
2022-02-12 23:26:30 -0500

    Create img.py

---
+++ img.py
@@ -0,0 +1,34 @@
+from tkinter import *  
+import PIL
+from PIL import ImageTk,Image
+import random
+
+def image():
+
+    pic = 'nasa_pics/NasaPic.jpg'
+    #pic = "nasa_pics/nasa3.jpg"
+    image = PIL.Image.open(pic)
+
+    Width,height = image.size
+    
+    if height >= 1094:
+        height = 1030
+        
+    newH = height
+
+    print(Width, newH)
+
+
+    root = Tk()
+    root.iconbitmap("ponyo.ico")
+    root.title("ponko")
+    canvas = Canvas(root, width = Width, height = newH)
+    canvas.pack()
+    img = ImageTk.PhotoImage(Image.open(pic))
+    print(pic)
+    canvas.create_image(0, 0, anchor=NW, image=img)
+    root.mainloop() 
+
+print("befor gard")
+if __name__ == "__main__":
+    image()
